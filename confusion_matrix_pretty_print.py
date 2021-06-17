2281eaf1e852901b7d5e0b88e6183165c57c4ec0
rogergermann <6461485+rogergermann@users.noreply.github.com>
2021-06-17 09:42:49 +0200

    Update confusion_matrix_pretty_print.py
    In: plot_confusion_matrix_from_data:
    - Removed statically set variables lw and figsize since the defaults are set with the arguments
    - lw forwarded to pretty_plot_confusion_matrix

--- confusion_matrix_pretty_print.py
+++ confusion_matrix_pretty_print.py
@@ -222,11 +222,9 @@ def plot_confusion_matrix_from_data(y_test, predictions, columns=None, annot=Tru
 
     confm = confusion_matrix(y_test, predictions)
     cmap = 'Oranges';
-    fz = 11;
-    figsize=[9,9];
     show_null_values = 2
     df_cm = DataFrame(confm, index=columns, columns=columns)
-    pretty_plot_confusion_matrix(df_cm, fz=fz, cmap=cmap, figsize=figsize, show_null_values=show_null_values, pred_val_axis=pred_val_axis)
+    pretty_plot_confusion_matrix(df_cm, fz=fz, lw=lw, cmap=cmap, figsize=figsize, show_null_values=show_null_values, pred_val_axis=pred_val_axis)
 #
 
 
